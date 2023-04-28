d50b1ea3aadebd2596ccd41c19a73887c775af9d
Yileri <yigit.ileri@ozu.edu.tr>
2023-04-28 17:55:16 +0300

    task 1 is done

--- Task 1/box.py
+++ Task 1/box.py
@@ -9,22 +9,22 @@ def max_nest(boxes):
             if float(boxes[j][0]) < float(boxes[j+1][0]):
                 boxes[j], boxes[j+1] = boxes[j+1], boxes[j]
 
-    print("*" * 100)
     print(boxes)
 
     dp = [1 for x in range(n)]
     for i in range(n):
         for j in range(i+1, n):
-            if boxes[i][0] < boxes[j][0] and boxes[i][1] < boxes[j][1] and boxes[i][2] < boxes[j][2]:
+            if float(boxes[i][0]) > float(boxes[j][0]) and float(boxes[i][1]) > float(boxes[j][1]) and float(boxes[i][2]) > float(boxes[j][2]):
                 dp[j] = max(dp[j], dp[i]+1)
 
+    print(dp)
     return max(dp)
 
 
 def take_input():
 
-    # testfile = sys.argv[1]
-    with open('test1.txt', 'r') as file:
+    testfile = sys.argv[1]
+    with open(testfile, 'r') as file:
         lines = file.readlines()
         n = int(lines[0])
         boxes = [0 for x in range(n)]
@@ -32,7 +32,6 @@ def take_input():
             box_dim = lines[i].strip().split()
             boxes[i-1] = box_dim
 
-    print(boxes)
     print(max_nest(boxes))
 
 
